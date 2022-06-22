f5e2f792a47c505a33885866eb2888bc5b4b76d8
John Piper <mrpiperpiper@gmail.com>
2022-06-22 22:18:54 +0100

    Added Documentation for glue_files_from_folder and df_headers_ok

--- R/glue_files_from_folder.R
+++ R/glue_files_from_folder.R
@@ -1,3 +1,29 @@
+#' Collects files from folder and joins the data into one data frame
+#'
+#' @param folder_path character vector.
+#' @param pattern character.  Regex pattern used in list.files.
+#' @param check_header_names logical. True to Check the files are the same by column names or False for number of columns
+#' @param first_file character vector. Choose the file name that all the other files check for the same column names or number of columns
+#' @param func.  The function to use to open the files from the folder.  Default is csv.read.  User could use external packages to open files
+#' @param ... Extra arguments can be added that will be passed into the func param.
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' df <- glue_from_folder("example_path/")
+#'
+#' df <- glue_from_folder("example_path/",
+#'                        "*example.csv",
+#'                        check_header_names = FALSE,
+#'                        first_file = "first_file.csv",
+#'                        func=read.csv,
+#'                        stringsAsFactors=FALSE
+#'                       )
+#' }
 glue_from_folder <- function(folder_path = "",
                              pattern = "*.csv",
                              check_header_names = TRUE,
